33b0bbbf90cce6382b7dab4d503cd02c93d617ed
Tiger <zhang_qi_tai@163.com>
2021-09-27 21:21:34 +0800

    fix: 清除$bus多次触发

--- src/views/home/components/DeviceOperation.vue
+++ src/views/home/components/DeviceOperation.vue
@@ -264,7 +264,7 @@ export default {
   },
   mounted() {
     this.getDeviceMessageInterface();
-    this.theTimer = setInterval(this.getDeviceMessageInterface, 30000);
+    this.theTimer = setInterval(this.getDeviceMessageInterface, 5000);
   },
   methods: {
     /**
